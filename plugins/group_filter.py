a97ce8b7c946d5a27f6a20df64e79e8e43552603
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-03-03 18:40:21 +0200

    Update group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -142,7 +142,7 @@ async def auto_filter(client, msg, spoll=False):
         temp.BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
-            [InlineKeyboardButton(text=f"📄 1/{math.ceil(int(total_results) / 6)}", callback_data="pages"),
+            [InlineKeyboardButton(text=f"الصفحات 1 📄/{math.ceil(int(total_results) / 6)}", callback_data="pages"),
              InlineKeyboardButton(text="⏩ التالي ", callback_data=f"next_{req}_{key}_{offset}")]
         )
     else:
