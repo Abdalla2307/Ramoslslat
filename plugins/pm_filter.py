321cfd6fffae0f6563fc85917cf8d38192482934
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-03-05 06:14:38 +0200

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -67,11 +67,11 @@ async def pm_AutoFilter(client, msg, pmspoll=False):
 
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
-        temp.BUTTONS[key] = search
+        temp.PM_BUTTONS[key] = search
         req = message.from_user.id if message.from_user else 0
         btn.append(
             [InlineKeyboardButton(text=f"📄 𝗣𝗮𝗴𝗲 1/{math.ceil(int(total_results) / 6)}", callback_data="pages"),
-             InlineKeyboardButton(text="𝗡𝗲𝘅𝘁 ➡️", callback_data=f"next_{req}_{key}_{offset}")]
+            InlineKeyboardButton(text="𝗡𝗲𝘅𝘁 ➡️", callback_data=f"pmnext_{req}_{key}_{offset}")]
         )
     else:
         btn.append(
