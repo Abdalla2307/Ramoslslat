3c148f9d04bf8a522be8f7d7ff0da0280a0aaa68
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-03-12 20:47:15 +0200

    Update next_and_spell.py

--- plugins/next_and_spell.py
+++ plugins/next_and_spell.py
@@ -74,20 +74,20 @@ async def next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
-            [InlineKeyboardButton("الحلقات الجديدة ⏪", callback_data=f"next_{req}_{key}_{off_set}"),
+            [InlineKeyboardButton("الحلقات الجديدة⏪", callback_data=f"next_{req}_{key}_{off_set}"),
              InlineKeyboardButton(f"📃 Pages {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}",
                                   callback_data="pages")]
         )
     elif off_set is None:
         btn.append(
             [InlineKeyboardButton(f"🗓 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
-             InlineKeyboardButton("⏩ الحلقات القديمة", callback_data=f"next_{req}_{key}_{n_offset}")])
+             InlineKeyboardButton("⏩الحلقات القديمة", callback_data=f"next_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
-                InlineKeyboardButton("الحلقات الجديدة ⏪", callback_data=f"next_{req}_{key}_{off_set}"),
+                InlineKeyboardButton("الحلقات الجديدة⏪", callback_data=f"next_{req}_{key}_{off_set}"),
                 InlineKeyboardButton(f"🗓 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
-                InlineKeyboardButton("⏩ الحلقات القديمة", callback_data=f"next_{req}_{key}_{n_offset}")
+                InlineKeyboardButton("⏩الحلقات القديمة", callback_data=f"next_{req}_{key}_{n_offset}")
             ],
         )
     try:
@@ -164,19 +164,19 @@ async def pm_next_page(bot, query):
         off_set = offset - 10
     if n_offset == 0:
         btn.append(
-            [InlineKeyboardButton("الحلقات الجديدة ⏪", callback_data=f"pmnext_{req}_{key}_{off_set}"),
+            [InlineKeyboardButton("الحلقات الجديدة⏪", callback_data=f"pmnext_{req}_{key}_{off_set}"),
              InlineKeyboardButton(f"📃 Pages {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages")]                                  
         )
     elif off_set is None:
         btn.append(
             [InlineKeyboardButton(f"🗓 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
-             InlineKeyboardButton("⏩ الحلقات القديمة", callback_data=f"pmnext_{req}_{key}_{n_offset}")])
+             InlineKeyboardButton("⏩الحلقات القديمة", callback_data=f"pmnext_{req}_{key}_{n_offset}")])
     else:
         btn.append(
             [
-                InlineKeyboardButton("الحلقات الجديدة ⏪", callback_data=f"pmnext_{req}_{key}_{off_set}"),
+                InlineKeyboardButton("الحلقات الجديدة⏪", callback_data=f"pmnext_{req}_{key}_{off_set}"),
                 InlineKeyboardButton(f"🗓 {math.ceil(int(offset) / 10) + 1} / {math.ceil(total / 10)}", callback_data="pages"),
-                InlineKeyboardButton("⏩ الحلقات القديمة", callback_data=f"pmnext_{req}_{key}_{n_offset}")
+                InlineKeyboardButton("⏩الحلقات القديمة", callback_data=f"pmnext_{req}_{key}_{n_offset}")
             ],
         )
     try:
