31a77d18727f7ee86639b38d369478d73e674389
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-03-22 03:29:54 +0200

    Create group_filter.py

--- plugins/group_filter.py
+++ plugins/group_filter.py
@@ -187,7 +187,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"<b><i>✅ المسلسل الذي بحثت عنه هو</i></b> 🔍 <_>{search}</_>"
+        cap = f"<b><i>✅ المسلسل الذي بحثت عنه هو</i></b>\n 🔍 <code>{search}</code>"
     if imdb and imdb.get('poster'):
         try:
             hehe = await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
