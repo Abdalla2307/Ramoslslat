31f27d93f6efa682c45a3de35f33a73385cb1763
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-03-22 15:30:48 +0200

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -515,6 +515,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await removebg_plain(client, query.message)
     elif query.data == "rmbgsticker":
         await removebg_sticker(client, query.message)
+    elif query.data == 'next':
+        await query.answer("hello", True)
     elif query.data == 'معلومة':
         await query.answer("لو مش لاقي المسلسل علي البوت اكتب اسمه في جروب الطلبات والبوت هيبعته تلقائي\n\n[طريقة البحث]\nمسلسل العمدة\nمسلسل سوق الكانتو\nمسلسل الكبير الحلقه 1\n", True)
     elif query.data == 'مش لاقي المسلسل':
