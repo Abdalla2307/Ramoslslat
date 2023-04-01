39b19eca71a665f14893edc10fc6adb50637f2e7
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-04-01 06:02:52 +0200

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -63,14 +63,14 @@ async def start(client, message):
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
-            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
+            telegram.ChatInviteLink = await client.create_chat_telegram.ChatInviteLink(int(AUTH_CHANNEL))
         except ChatAdminRequired:
             logger.error("Make sure Bot is admin in Forcesub channel")
             return
         btn = [
             [
                 InlineKeyboardButton(
-                    "🤖 Join Updates Channel", url=invite_link.invite_link
+                    "🤖 Join Updates Channel", url=telegram.ChatInviteLink.telegram.ChatInviteLink
                 )
             ]
         ]
