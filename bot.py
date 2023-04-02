a26492f713e8cb1894906c038cb2e887089597a3
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-04-02 03:25:21 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from database.users_chats_db import db
 from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_CHANNEL, PORT, WEBHOOK
 from utils import temp, __repo__, __license__, __copyright__
 from typing import Union, Optional, AsyncGenerator
-from pyrogram import types
+from pyrogram import types, raw
 from datetime import datetime
 from pytz import timezone
 from pyrogram.errors import BadRequest, Unauthorized
@@ -68,7 +68,13 @@ class Bot(Client):
             except BadRequest as e:
                 LOGGER.error(e)
                                          
-
+class GetChatJoinRequests:
+    async def get_chat_join_requests(
+        self: "pyrogram.Client",
+        chat_id: Union[int, str],
+        limit: int = 0,
+        query: str = ""
+    ) -> Optional[AsyncGenerator["types.ChatJoiner", None]]:
     async def stop(self, *args):
         await super().stop()
         me = await self.get_me()
@@ -84,7 +90,40 @@ class Bot(Client):
             for message in messages:
                 yield message
                 current += 1
+        current = 0
+        total = abs(limit) or (1 << 31) - 1
+        limit = min(100, total)
 
+        offset_date = 0
+        offset_user = raw.types.InputUserEmpty()
+
+        while True:
+            r = await self.invoke(
+                raw.functions.messages.GetChatInviteImporters(
+                    peer=await self.resolve_peer(chat_id),
+                    limit=limit,
+                    offset_date=offset_date,
+                    offset_user=offset_user,
+                    requested=True,
+                    q=query
+                )
+            )
+
+            if not r.importers:
+                break
+
+            users = {i.id: i for i in r.users}
+
+            offset_date = r.importers[-1].date
+            offset_user = await self.resolve_peer(r.importers[-1].user_id)
+
+            for i in r.importers:
+                yield types.ChatJoiner._parse(self, i, users)
+
+                current += 1
+
+                if current >= total:
+                    return
 
         
 app = Bot()
