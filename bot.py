577ea72b4542049518ae29142133f36cfd865851
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-04-02 05:37:10 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,16 +74,62 @@ class Bot(Client):
         me = await self.get_me()
         logging.info(f"{me.first_name} is_...  ♻️Restarting...")
 
-    async def iter_messages(self, chat_id: Union[int, str], limit: int, offset: int = 0) -> Optional[AsyncGenerator["types.Message", None]]:                       
-        current = offset
+    async def iter_messages(
+        self: "pyrogram.Client",
+        chat_id: Union[int, str],
+        limit: int = 0,
+        query: str = ""
+    ) -> Optional[AsyncGenerator["types.ChatJoiner", None]]:
+        """Get the pending join requests of a chat.
+        .. include:: /_includes/usable-by/users-bots.rst
+        Parameters:
+            chat_id (``int`` | ``str``):
+                Unique identifier for the target chat or username of the target channel/supergroup
+                (in the format @username).
+            limit (``int``, *optional*):
+                Limits the number of invite links to be retrieved.
+                By default, no limit is applied and all invite links are returned.
+            query (``str``, *optional*):
+                Query to search for a user.
+        Returns:
+            ``Generator``: A generator yielding :obj:`~pyrogram.types.ChatJoiner` objects.
+        Yields:
+            :obj:`~pyrogram.types.ChatJoiner` objects.
+        """
+        current = 0
+        total = abs(limit) or (1 << 31) - 1
+        limit = min(100, total)
+
+        offset_date = 0
+        offset_user = raw.types.InputUserEmpty()
+
         while True:
-            new_diff = min(200, limit - current)
-            if new_diff <= 0:
-                return
-            messages = await self.get_messages(chat_id, list(range(current, current+new_diff+1)))
-            for message in messages:
-                yield message
+            r = await self.invoke(
+                raw.functions.messages.GetChatInviteImporters(
+                    peer=await self.resolve_peer(chat_id),
+                    limit=limit,
+                    offset_date=offset_date,
+                    offset_user=offset_user,
+                    requested=True,
+                    q=query
+                )
+            )
+
+            if not r.importers:
+                break
+
+            users = {i.id: i for i in r.users}
+
+            offset_date = r.importers[-1].date
+            offset_user = await self.resolve_peer(r.importers[-1].user_id)
+
+            for i in r.importers:
+                yield types.ChatJoiner._parse(self, i, users)
+
                 current += 1
+
+                if current >= total:
+                    return
 
 
         
