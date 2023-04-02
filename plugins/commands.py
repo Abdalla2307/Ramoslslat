a0f4858339674be17a50388de47a82021dbc9292
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-04-02 03:46:43 +0200

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -63,14 +63,14 @@ async def start(client, message):
         return
     if AUTH_CHANNEL and not await is_subscribed(client, message):
         try:
-            join_request = await client.creates_join_request(int(AUTH_CHANNEL))
+            invite_link = await client.create_chat_invite_link(int(AUTH_CHANNEL))
         except ChatAdminRequired:
             logger.error("Make sure Bot is admin in Forcesub channel")
             return
         btn = [
             [
                 InlineKeyboardButton(
-                    "🤖 Join Updates Channel", url=join_request.join_request
+                    "🤖 Join Updates Channel", url=invite_link.invite_link
                 )
             ]
         ]
