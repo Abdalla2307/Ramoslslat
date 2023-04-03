9bb1edccd38553d12142afe684bb8a815a502a32
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-04-03 06:49:42 +0200

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -308,7 +308,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
     elif query.data.startswith("checksub"):
         if AUTH_CHANNEL and not await is_subscribed(client, query):
-            await query.answer("I Like Your Smartness, But Don't Be Oversmart Okay", show_alert=True)
+            await query.answer("بعد تخطي الرابط هيبعتلك لينك قناة انضم ليها عشان تقدر تشوف المسلسلات", show_alert=True)
             return
         ident, file_id = query.data.split("#")
         files_ = await get_file_details(file_id)
