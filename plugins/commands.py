5cc32a9cd538e58f179b25d267223744dc0e6a9e
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-04-06 06:28:44 +0200

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -68,9 +68,10 @@ async def start(client, message):
             logger.error("Make sure Bot is admin in Forcesub channel")
             return
         btn = [[
-                InlineKeyboardButton("🤖 اسـتـخـدام الـبـــوت 🤖", url='https://short-jambo.com/i4FyA')
+                InlineKeyboardButton("💥 اسـتـخـدام الـبـــوت 💥", url='https://short-jambo.com/i4FyA')
         ], [ 
-                InlineKeyboardButton("💥 طــريـقـة تخـطــي الـرابــط 💥", url='https://t.me/RamadanTV7')
+                InlineKeyboardButton("⚡طريقة تخـطي الرابط⚡", url='https://t.me/RamadanTV7'),
+                InlineKeyboardButton("ليه لازم اتخطي الرابط!", callback_data='leeh')
         ]]
 
 
@@ -78,11 +79,9 @@ async def start(client, message):
             try:
                 kk, file_id = message.command[1].split("_", 1)
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}"),
-                InlineKeyboardButton("ليه لازم اتخطي الرابط!", callback_data='leeh')])
+                btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}")])
             except (IndexError, ValueError):
-                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}"),
-                InlineKeyboardButton("ليه لازم اتخطي الرابط!", callback_data='leeh')]) 
+                btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}")]) 
         await client.send_message(
             chat_id=message.from_user.id,
             text=FORCE_SUB_TEXT,
