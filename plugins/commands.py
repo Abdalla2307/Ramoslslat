c75c297fb49879bc2bf130f71627d81a943e42bf
Abdalla2307 <99101601+Abdalla2307@users.noreply.github.com>
2023-04-06 07:01:31 +0200

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -79,10 +79,10 @@ async def start(client, message):
                 kk, file_id = message.command[1].split("_", 1)
                 pre = 'checksubp' if kk == 'filep' else 'checksub' 
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", callback_data=f"{pre}#{file_id}"),
-                InlineKeyboardButton('ليه لازم اتخطي الرابط!', callback_data='leh')])
+            InlineKeyboardButton('ليه لازم اتخطي الرابط!', callback_data='leeeh')])
             except (IndexError, ValueError):
                 btn.append([InlineKeyboardButton(" 🔄 Try Again", url=f"https://t.me/{temp.U_NAME}?start={message.command[1]}"),
-                InlineKeyboardButton('ليه لازم اتخطي الرابط!', callback_data='leh')]) 
+            InlineKeyboardButton('ليه لازم اتخطي الرابط!', callback_data='leeeh')])
         await client.send_message(
             chat_id=message.from_user.id,
             text=FORCE_SUB_TEXT,
